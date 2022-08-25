2fb70f65cb8bd2b0515b184d17a3cbba85f6d293
man90 <man90@tuta.io>
2022-08-25 08:56:44 +0200

    Disable crashpad

---
+++ etc/cron.daily/disable-crashpad.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+chmod 644 /opt/brave/chrome_crashpad_handler
+chmod 644 /opt/slack/chrome_crashpad_handler
+chmod 644 /opt/vscodium/chrome_crashpad_handler
+chmod 644 /opt/google/chrome/chrome_crashpad_handler
