852c017cec6b1050ed67df14a01b843ce9e97c30
man90 <man90@tuta.io>
2023-07-25 22:01:32 +0200

    Remove slack from disable-crashpad script

--- etc/cron.daily/disable-crashpad.sh
+++ etc/cron.daily/disable-crashpad.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 chmod 644 /opt/brave/chrome_crashpad_handler
-chmod 644 /opt/slack/chrome_crashpad_handler
 chmod 644 /opt/vscodium/chrome_crashpad_handler
 chmod 644 /opt/google/chrome/chrome_crashpad_handler
